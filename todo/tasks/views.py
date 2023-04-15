40c0d2fecc18a0b94e3e80a10017dddbaefbc96c
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-15 19:17:09 +0300

    Tasks

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    context = {'tasks': tasks}
+    return render(request, 'list.html', context)
