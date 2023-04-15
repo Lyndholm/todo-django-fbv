9cd22f856bd1e202c0a7f6069d017fce606e6955
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-15 19:26:23 +0300

    Create new task form

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Task
+from .forms import CreateTaskForm
 
 
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks': tasks}
+    form = CreateTaskForm()
+
+    if request.method == 'POST':
+        form = CreateTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'list.html', context)
